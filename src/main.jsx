f41e285e12f9e9014c043aed87e8d7e67d1fb215
Olena Oleksandrova <ha240886kev@gmail.com>
2024-12-03 00:19:01 +0200

    refactoring code + css

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,24 @@ import React from "react";
 // import ReactDOM from "react-dom";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import store, { persistor } from "./redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
+// import store, { persistor } from "./redux/store";
 import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+//         <App />
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,38 +4,47 @@ import filtersReducer from "./filtersSlice";
 
 // Налаштування store
 // з лекції частина 1
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
 
-const persistConfig = {
-  key: 'contacts',
-  version: 1,
-  storage,
-}
+// const persistConfig = {
+//   key: 'contacts',
+//   version: 1,
+//   storage,
+// }
 
-const persistedReducer = persistReducer(persistConfig, contactsReducer)
+// const persistedReducer = persistReducer(persistConfig, contactsReducer)
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistedReducer,
+//     filters: filtersReducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export default store;
+// export let persistor = persistStore(store);
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-}); 
+});
 
 export default store;
-export let persistor = persistStore(store);

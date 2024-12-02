056826bc37f25d05d8a2d1146f0058562400831c
Olena Oleksandrova <ha240886kev@gmail.com>
2024-12-02 23:15:11 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectFilter } from "../../redux/filtersSlice";
+import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css";
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  // const contacts = useSelector(state => state.contacts.items)
+  const filter = useSelector(selectFilter);
+  // const filter = useSelector(state => state.contacts.filter)
+  
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={s.list}>
+      {filteredContacts.map(({ id, name, number }) => (
+        <Contact key={id} id={id} name={name} number={number} />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
